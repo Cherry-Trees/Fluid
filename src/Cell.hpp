db24e7e3f40120f7b0166070417776e726b5a787
Cherry-Trees <147207363+Cherry-Trees@users.noreply.github.com>
2024-03-04 23:22:17 -0600

    Update Cell.hpp

--- src/Cell.hpp
+++ src/Cell.hpp
@@ -6,7 +6,7 @@
 void Cell::update(sf::RenderWindow &window)
 {
     sprite.setFillColor(sf::Color(std::max(std::min(q, 255.f), 10.f), 
-                                        std::max(std::min(q/6, 255.f), 10.f), 
-                                        std::max(std::min(q/3, 255.f), 10.f)));
+                                  std::max(std::min(q/6, 255.f), 10.f), 
+                                  std::max(std::min(q/3, 255.f), 10.f)));
     window.draw(sprite);
 }
